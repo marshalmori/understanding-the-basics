f14b25361e652af8784ba9a312eedadf37311425
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-08-17 16:30:20 -0300

    Installing Express.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
