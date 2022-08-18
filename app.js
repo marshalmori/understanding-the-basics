4262920872cc7119de2832b4850364bf1938c69e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-08-17 21:43:00 -0300

    Handling different routes

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware 2!");
+  res.send("<h1>Hello from Express 2!</h1>");
 });
 
 app.listen(3000);
