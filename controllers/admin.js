c69927cb0ef7e1e029046879f3414a4c8a287504
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-09-20 20:52:16 -0300

    Using query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,12 +2,9 @@ const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 };
 
@@ -22,6 +19,18 @@ exports.postAddProduct = (req, res, next) => {
 
   // res.redirect("/products");
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  res.render("admin/edit-product", {
+    pageTitle: "Edit product",
+    path: "/admin/edit-product",
+    editing: editMode,
+  });
 };
 
 exports.getProducts = (req, res, next) => {
