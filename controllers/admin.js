cdb38bd51a393cc9d9e828d2666656b11d408b0f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-09-21 15:31:09 -0300

    Populating datas

--- controllers/admin.js
+++ controllers/admin.js
@@ -26,10 +26,17 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit product",
-    path: "/admin/edit-product",
-    editing: editMode,
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!editMode) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
